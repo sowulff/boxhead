1994f6f30557b79e15fa91757277633393fcd556
DavisDavisDavis <olizoooom@gmail.com>
2022-05-27 22:17:15 +0200

    Checkout and rebase from master to weapons

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import setUpKeys from './scripts/player/Controller';
 import { Loader } from 'pixi.js';
 import Game from './scripts/Game';
+import Zombie from './scripts/enemies/Zombie';
+import Pistol from './scripts/weapons/Weapons';
 
 setUpKeys();
 
